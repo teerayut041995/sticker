8966eebbec9f0487164c6bad766a4a50379671ec
teerayut041995 <teerayut041995@gmail.com>
2020-11-16 02:10:21 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sticker';
+
+  public select: Sticker;
+
+  public sticker: Sticker[] = [
+    {
+      id: '1',
+      name: 'หิว',
+      path: '/assets/images/sticker-1.gif'
+    },
+    {
+      id: '2',
+      name: 'ดีใจ',
+      path: '/assets/images/sticker-2.gif'
+    },
+    {
+      id: '2',
+      name: 'โกรธ',
+      path: '/assets/images/sticker-3.gif'
+    }
+  ];
+  public image: string = '/assets/images/sticker-1.gif';
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  selectSticker(sticker) {
+    this.select = sticker;
+    console.log(this.select);
+  }
+
+  clearSticker() {
+    this.select = null;
+  }
+
+}
+
+interface Sticker {
+  id: string,
+  name: string,
+  path: string
 }
